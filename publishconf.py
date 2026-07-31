182f2fd0233ca448ac546e5e8828206da8068e96
sharan <shrnsrnvsn14@gmail.com>
2026-07-31 13:55:50 +0100

    url fix 2

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "infrequentblogs.dev"
+SITEURL = "https://srinivasan-sharan.github.io/infrequent-logs/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
